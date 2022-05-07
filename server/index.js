8ee38e3fbad31955b276bd527e582f48b8b6fe57
byp0409 <1024567199@qq.com>
2022-05-07 12:19:57 +0800

    服务器

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
 const router = require('./router');
-//引入中间件
+//引入中间件  允许接收post请求
 const bodyParser = require('body-parser');
 
 // 请求体解析中间件
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json()); // parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
 
 app.use('/api', router);
 
