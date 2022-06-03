24cc4d199a2cd27b6f0f894f9a95fe1f6f71b3a7
byp0409 <1024567199@qq.com>
2022-06-03 21:10:21 +0800

    配置

--- vue.config.js
+++ vue.config.js
@@ -2,13 +2,15 @@ const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   lintOnSave: false,
   transpileDependencies: true,
+  // 打包无map
+  productionSourceMap: false,
   // 配置代理服务器
   devServer: {
     host: 'localhost',
     port: '8080', // 设置端口号
     proxy: {
       '/api': {
-        target: 'http://8.130.23.181:8090', //API服务器的地址
+        target: 'http://123.57.195.238:9090', //API服务器的地址
         ws: true, //代理websockets
         changeOrigin: true, // 是否跨域，虚拟的站点需要更管origin
         pathRewrite: {
